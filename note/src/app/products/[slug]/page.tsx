efa438eaa3a865ca314fb240a247ea9066a94222
jungjuyoung <jungjuyoung02@gmail.com>
2023-08-17 15:27:39 +0900

    dynamic 라우팅 페이지에서 SSG 만들때 설명문구 추가

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -11,6 +11,8 @@ const pantsPage = ({params} : Props) => {
 }
 export default pantsPage
 
+// 동적 라우팅의 경우,  SSR이기 때문에 미리 빌드시에 SSG하고 싶다면 
+// 특정경로를 params로 넘겨서 만들어 둔다.
 export function generateStaticParams() {
   const products = ['pants', 'skirt']
   return products.map(product => ({
