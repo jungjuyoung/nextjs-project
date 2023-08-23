a4298d650272dc792f65eab36a1242879c2eb49a
jungjuyoung <nwwn1004@naver.com>
2023-08-23 22:21:59 +0900

    SSG rendering

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -7,29 +7,30 @@ type Props = {
   } 
 }
 
-export function generateMetadata({ params }: Props) {
+export function generateMetadata({ params: { slug } }: Props) {
   return {
-    title: `제품의 이름: ${params.slug}`
+    title: `제품의 이름: ${slug}`
   }
 }
-const pantsPage = ({ params: { slug } }: Props) => {
-  const product = getProduct(slug);
+const productPage = async ({ params: { slug } }: Props) => {
+  const product = await getProduct(slug);
   if (!product) { 
     notFound();
   }
   // 서버파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌.
   return (
-    <div>{product} 상세 페이지</div>
+    <div>{product.name} 상세 페이지</div>
   )
 }
-export default pantsPage
+export default productPage
 
 // 동적 라우팅의 경우,  SSR이기 때문에 미리 빌드시에 SSG하고 싶다면 
 // 특정경로를 params로 넘겨서 만들어 둔다.
-export function generateStaticParams() {
-  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄것임( 현재는 규모가 작음으로...) SSG로.
-  const products = getProducts()
+export async function generateStaticParams() {
+  // 일단 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄것임( 현재는 규모가 작음으로...) SSG로.
+  const products = await getProducts();
+  console.log(products)
   return products.map(product => ({
-    slug: product
+    slug: product.id
   }))
 }
--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,15 +1,15 @@
-import { getProducts } from '@/utils/products';
+import { getProducts } from '@/utils/products'
 import Link from 'next/link'
 import React from 'react'
 
-const productPage = () => {
-  const products = getProducts();
+const productPage = async () => {
+  const products = await getProducts();
   // 서버파일(DB)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌. 
   return (
     <>
       <h1>제품 소개페이지</h1>
       <ul>
-        {products.map((product, index) => <li key={index}><Link href={`products/${product}`}>{ product}</Link></li>)}
+        {products.map(({id, name}, index) => <li key={index}><Link href={`products/${id}`}>{name}</Link></li>)}
       </ul>
       </>
   )
