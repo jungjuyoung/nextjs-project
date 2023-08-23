94102ed1feaa0e799bcbf2723d02e02435fe3b77
jungjuyoung <nwwn1004@naver.com>
2023-08-23 23:05:32 +0900

    ISR 설정하는 방법

--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -2,6 +2,8 @@ import { getProducts } from '@/utils/products'
 import Link from 'next/link'
 import React from 'react'
 
+export const revalidate = 3
+
 const productPage = async () => {
   const products = await getProducts();
   // 서버파일(DB)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌. 
