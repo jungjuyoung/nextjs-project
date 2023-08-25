50b90ebea58e3f38df6d24a3ed85bb5c9c3dcb43
jungjuyoung <nwwn1004@naver.com>
2023-08-25 23:37:49 +0900

    ISR, SSG, SSR렌더링 방식의 차이

--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -2,12 +2,15 @@ import { getProducts } from '@/utils/products';
 import Link from 'next/link';
 import React from 'react';
 
-// export const revalidate = 3; // 3초 간격으로 ISR
+// export const revalidate = 3; // 3초 간격으로 ISR (page에서)
 
 const productPage = async () => {
   const products = await getProducts();
   // 서버파일(DB)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌.
-  const res = await fetch('https://meowfacts.herokuapp.com/', { next: { revalidate: 3 } });
+  const res = await fetch('https://meowfacts.herokuapp.com/', { next: { revalidate: 0 } }); // 3초 간격으로 ISR을 fetch단위에서
+  // { next: { revalidate: 0 } } revalidate: 0은 (SSR),
+  // cashe: force-cache가 기본값 (SSG)
+  // cache: no-store도 (SSR)
   const data = await res.json();
   const factText = data.data[0];
   // console.log(factText);
