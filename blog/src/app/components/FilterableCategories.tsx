23bff29001321bff64b3d41bd52c4014957b9ca4
jungjuyoung <nwwn1004@naver.com>
2023-09-06 01:03:03 +0900

    posts 만들기

---
+++ blog/src/app/components/FilterableCategories.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+import { Posts } from "../utils/posts";
+import PostsGrid from "./PostsGrid";
+import Categories from "./Categories";
+type Props = {
+  posts: Posts[];
+  categories: string[];
+};
+const ALL_POSTS = "All Posts";
+const FilterableCategories = ({ posts, categories }: Props) => {
+  console.log("posts", posts, "categories", categories);
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filtered =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+  return (
+    <section>
+      <PostsGrid posts={filtered} />
+      <Categories
+        categories={[ALL_POSTS, ...categories]}
+        selected={selected}
+        onClick={setSelected}
+      />
+    </section>
+  );
+};
+
+export default FilterableCategories;
--- blog/src/app/posts/page.tsx
+++ blog/src/app/posts/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { getAllPosts } from "../utils/posts";
+import FilterableCategories from "../components/FilterableCategories";
 
-const PostsPage = () => {
-  return <div>포스트들...</div>;
+const PostsPage = async () => {
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map((post) => post.category))];
+  return <FilterableCategories posts={posts} categories={categories} />;
 };
 
 export default PostsPage;
