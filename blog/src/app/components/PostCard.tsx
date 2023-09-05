bf0397bb2b1671d13e10dcfc1f716b68bbd80816
jungjuyoung <nwwn1004@naver.com>
2023-09-05 15:26:25 +0900

    grid 형식의 Featured post 완성

--- blog/src/app/components/PostCard.tsx
+++ blog/src/app/components/PostCard.tsx
@@ -11,18 +11,23 @@ const PostCard = ({
 }: Props) => {
   return (
     <Link href={`/posts/${path}`}>
-      <Image
-        src={`/images/posts/${path}.png`}
-        alt={title}
-        width={300}
-        height={200}
-      />
-      <div>
-        <time>{date.toString()}</time>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <span>{category}</span>
-      </div>
+      <article className="rounded-md overflow-hidden shadow-md">
+        <Image
+          className="w-full"
+          src={`/images/posts/${path}.png`}
+          alt={title}
+          width={300}
+          height={200}
+        />
+        <div className="flex flex-col items-center p-4">
+          <time className="self-end">{date.toString()}</time>
+          <h3 className="text-xl font-bold">{title}</h3>
+          <p className="w-full truncate text-center">{description}</p>
+          <span className="text-sm rounded-lg bg-green-300 px-2 my-2">
+            {category}
+          </span>
+        </div>
+      </article>
     </Link>
   );
 };
