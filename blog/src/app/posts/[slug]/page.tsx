9cd213153e71522e362cfdd49d0d2b8c74acb8ff
jungjuyoung <nwwn1004@naver.com>
2023-09-13 01:08:23 +0900

    마크다운뷰어설치 마크다운 뷰어 플러그인 설치

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import MarkDownViewer from "@/app/components/MarkdownViewer";
 import { getPostData } from "@/app/utils/posts";
 import React from "react";
 type Props = {
@@ -6,13 +7,11 @@ type Props = {
   };
 };
 const postPage = async ({ params: { slug } }: Props) => {
-  // 1. slug에 전달된 path를 읽어와서
-  // 2. path를 마크다운 뷰어에 표기.
   const post = await getPostData(slug);
   return (
     <div>
       <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
+      <MarkDownViewer content={post.content} />
     </div>
   );
 };
