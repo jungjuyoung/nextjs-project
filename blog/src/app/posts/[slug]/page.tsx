99fc76ebe3e0d3070db1123454e87fe5b3764bfe
jungjuyoung <nwwn1004@naver.com>
2023-09-13 21:19:51 +0900

    utils posts 비지니스로직변경

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -1,6 +1,6 @@
-import PostContent from "@/app/components/PostContent";
-import { getPostData } from "@/app/utils/posts";
-import Image from "next/image";
+import PostContent from '@/app/components/PostContent';
+import { getPostData } from '@/app/utils/posts';
+import Image from 'next/image';
 
 type Props = {
   params: {
@@ -9,19 +9,16 @@ type Props = {
 };
 const postPage = async ({ params: { slug } }: Props) => {
   const post = await getPostData(slug);
-  const { title, path } = post;
+  const { title, path, next, prev } = post;
   // console.log(typeof date, date);
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
-      <Image
-        className="w-full h-1/5 max-h-[500px] object-fit"
-        src={`/images/posts/${path}.png`}
-        alt={title}
-        width={760}
-        height={420}
-      />
+      <Image className="w-full h-1/5 max-h-[500px] object-fit" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
       <PostContent post={post} />
-      <section>이전 | 다음</section>
+      <section>
+        {next && <span>이전 {next.title}</span>}
+        {prev && <span>다음 {prev.title}</span>}
+      </section>
     </article>
   );
 };
--- blog/src/app/utils/posts.ts
+++ blog/src/app/utils/posts.ts
@@ -1,7 +1,7 @@
 import { readFile } from "fs/promises";
 import path from "path";
 
-export interface Posts {
+export interface Post {
   title: string;
   description: string;
   date: string;
@@ -9,33 +9,41 @@ export interface Posts {
   path: string
   featured: boolean
 }
-export interface PostData extends Posts {
+export interface PostData extends Post {
   content: string
+  next: Post | null
+  prev: Post | null
 }
 
-export function getFeaturedPosts(): Promise<Posts[]> {
+export function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts() // 아래에서 정의한 모든 포스트를 가져와서 featured된것만 필터링
     .then(posts => posts.filter(post => post.featured))
 }
 
-export function getNonFeaturedPosts(): Promise<Posts[]> {
+export function getNonFeaturedPosts(): Promise<Post[]> {
   return getAllPosts() // 아래에서 정의한 모든 포스트를 가져와서 Non Featured된것만 필터링
     .then(posts => posts.filter(post => !post.featured))
 }
 
-export async function getAllPosts(): Promise<Posts[]> {
+export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), 'data', 'posts.json')
-  return readFile(filePath, 'utf-8').then<Posts[]>(JSON.parse).then(posts => posts.sort((a, b) => (a.date > b.date ? -1 : 1)))
+  return readFile(filePath, 'utf-8').then<Post[]>(JSON.parse).then(posts => posts.sort((a, b) => (a.date > b.date ? -1 : 1)))
 }
 
 export async function getPostData(fileName: string): Promise<PostData> {
-  const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`)
-  // console.log('filePath', filePath)
-  const metaData = await getAllPosts().then(posts => posts.find(post => post.path === fileName))
-  if (!metaData) throw new Error(`${fileName}에 해당하는 포스트가 없습니다.`)
+  const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
+  const posts = await getAllPosts();
+  const post = posts.find(post => post.path === fileName);
+
+  if (!post) throw new Error(`${fileName}에 해당하는 포스트가 없습니다.`);
+
+  const index = posts.indexOf(post)
+  // console.log(index) // 2
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length - 1 ? posts[index + 1] : null;
   const content = await readFile(filePath, 'utf-8')
   return {
-    ...metaData,
-    content
+    ...post,
+    content, next, prev
   }
 }
