f77461308f935ac7fb4225619fbe127c474546b0
jungjuyoung <nwwn1004@naver.com>
2023-09-13 13:56:18 +0900

    마크다운 완료

--- blog/src/app/components/MarkdownViewer.tsx
+++ blog/src/app/components/MarkdownViewer.tsx
@@ -1,13 +1,48 @@
+"use client";
+
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 type Props = {
   content: string;
 };
 const MarkDownViewer = ({ content }: Props) => {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose lg:prose-xl max-w-none"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={dark}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-60 object-cover"
+            src={image.src || ""}
+            alt={image.alt || "image"}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
