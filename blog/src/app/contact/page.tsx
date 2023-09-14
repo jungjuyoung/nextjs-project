7d71236b71c2de2deabb3cc492e3697fc8de4df1
jungjuyoung <nwwn1004@naver.com>
2023-09-14 23:02:13 +0900

    contact me page and contactForm component

--- blog/src/app/contact/page.tsx
+++ blog/src/app/contact/page.tsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import ContactForm from "../components/ContactForm";
+const LINKS = [
+  { icon: <AiFillGithub />, url: "" },
+  { icon: <AiFillLinkedin />, url: "" },
+  { icon: <AiFillYoutube />, url: "" },
+];
+const TITLE_CLASS = "text-2xl font-bold my-2";
 const ContactPage = () => {
-  return <div>연락처</div>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className={TITLE_CLASS}>Contact Me</h2>
+      <p>nwwn1004@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-3xl hover:text-yellow-500 my-4"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className={TITLE_CLASS}>Or, Send me an email!!</h2>
+      <ContactForm />
+    </section>
+  );
 };
 
 export default ContactPage;
