bcd473590762b986e0d2c0e570494787ac2fddd2
jungjuyoung <nwwn1004@naver.com>
2023-09-14 23:39:24 +0900

    contactForm컴포넌트 개발완료

--- blog/src/app/components/ContactForm.tsx
+++ blog/src/app/components/ContactForm.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+"use client";
 
+import { ChangeEvent, FormEvent, useState } from "react";
+
+type EmailForm = {
+  from: string;
+  subject: string;
+  message: string;
+};
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  const [form, setForm] = useState<EmailForm>({
+    from: "",
+    subject: "",
+    message: "",
+  });
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+    console.log("name: ", name, "value: ", value);
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="from">Your email</label>
+      <input
+        type="email"
+        id="from"
+        name="from"
+        required
+        autoFocus
+        value={form.from}
+        onChange={onChange}
+      />
+      <label htmlFor="subject">Subject</label>
+      <input
+        type="text"
+        id="subject"
+        name="subject"
+        required
+        value={form.subject}
+        onChange={onChange}
+      />
+      <label htmlFor="message">Message</label>
+      <textarea
+        rows={10}
+        id="message"
+        name="message"
+        required
+        value={form.message}
+        onChange={onChange}
+      />
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default ContactForm;
