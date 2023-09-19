24d9cdf70e883254179ac65dc999d14c1135b480
jungjuyoung <nwwn1004@naver.com>
2023-09-20 00:43:56 +0900

    created user Schema

--- instagram/sanity-studio/schemas/user.js
+++ instagram/sanity-studio/schemas/user.js
@@ -1,1 +1,32 @@
-export default {}
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'documnet',
+  fields: [
+    {title: 'Username', name: 'username', type: 'string'},
+    {title: 'Name', name: 'name', type: 'string'},
+    {title: 'NaEmailme', name: 'email', type: 'string'},
+    {title: 'Image', name: 'image', type: 'string'},
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'user'}]}],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Followers',
+      name: 'followers',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'user'}]}],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'post'}]}],
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+}
