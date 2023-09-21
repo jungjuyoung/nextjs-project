eb74b7ef08667e1392af2357a14742c8518b9d46
jungjuyoung <nwwn1004@naver.com>
2023-09-21 21:44:31 +0900

    header complete

--- instagram/src/app/components/ui/ColorButton.tsx
+++ instagram/src/app/components/ui/ColorButton.tsx
@@ -7,7 +7,7 @@ const ColorButton = ({ text, onClick }: Props) => {
   return (
     <div className="rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-500 p-[0.15rem]">
       <button
-        className="rounded-sm text-base bg-white   p-[0.3rem] hover:opacity-20 transition-opacity"
+        className="rounded-sm text-base bg-white p-[0.3rem] hover:opacity-90 transition-opacity"
         onClick={onClick}
       >
         {text}
