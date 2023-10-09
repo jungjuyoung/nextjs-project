d2d557b0e97310daad2bea86a0535e7ce7fc1ca8
jungjuyoung <nwwn1004@naver.com>
2023-10-09 14:33:46 +0900

    styleing complement

--- instagram/src/app/components/UserCard.tsx
+++ instagram/src/app/components/UserCard.tsx
@@ -9,12 +9,15 @@ const UserCard = ({
   user: { name, username, image, followers, following },
 }: Props) => {
   return (
-    <Link href={`/user/${username}`}>
+    <Link
+      href={`/user/${username}`}
+      className="flex items-center w-full rounded-sm border border-neutral-300 mb-2 p-4 bg-white hover:bg-neutral-50"
+    >
       <Aavatar image={image} />
-      <div>
-        <p>{username}</p>
+      <div className="text-neutral-500">
+        <p className="text-black font-bold leading-4">{username}</p>
         <p>{name}</p>
-        <p>{`${followers} followers ${following} followings`}</p>
+        <p className="text-sm leading-4">{`${followers} followers ${following} followings`}</p>
       </div>
     </Link>
   );
--- instagram/src/app/components/UserSearch.tsx
+++ instagram/src/app/components/UserSearch.tsx
@@ -17,28 +17,31 @@ const UserSearch = () => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        autoFocus
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-        placeholder="Search for a useername or name"
-      />
-      {error && <p>무언가가 잘못되었음.</p>}
-      {isLoading && <GridLoader />}
-      {!isLoading && !error && users?.length === 0 && (
-        <p>찾는 사용자가 없음.</p>
-      )}
-      <ul>
-        {users &&
-          users?.map((user) => (
-            <li key={user.username}>
-              <UserCard user={user} />
-            </li>
-          ))}
-      </ul>
-    </form>
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form onSubmit={onSubmit} className="w-full mb-4">
+        <input
+          className="w-full text-xl p-3 outline-none border border-gray-400"
+          type="text"
+          autoFocus
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Search for a useername or name"
+        />
+        {error && <p>무언가가 잘못되었음.</p>}
+        {isLoading && <GridLoader />}
+        {!isLoading && !error && users?.length === 0 && (
+          <p>찾는 사용자가 없음.</p>
+        )}
+        <ul className="w-full p-4">
+          {users &&
+            users?.map((user) => (
+              <li key={user.username}>
+                <UserCard user={user} />
+              </li>
+            ))}
+        </ul>
+      </form>
+    </section>
   );
 };
 
