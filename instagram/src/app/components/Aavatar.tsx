1e11468c7608080ff32539b9ff345a8c59729d70
jungjuyoung <nwwn1004@naver.com>
2023-10-11 00:17:01 +0900

    refactoring for Aavatar component

--- instagram/src/app/components/Aavatar.tsx
+++ instagram/src/app/components/Aavatar.tsx
@@ -11,35 +11,27 @@ const getContainerStyle = (size: AavatarSize, highlight: boolean): string => {
   const highlightStyle = highlight
     ? "bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-500"
     : "";
-  const sizeStyle = getContainerSize(size);
-  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+  const { container } = getImageSizeStyle(size);
+  return `${baseStyle} ${highlightStyle} ${container}`;
 };
 
-const getContainerSize = (size: AavatarSize): string => {
-  switch (size) {
-    case "small":
-      return "w-9 h-9";
-    case "medium":
-      return "w-11 h-11";
-    case "large":
-      return "w-[68px] h-[68px]";
-    case "xlarge":
-      return "w-[142px] h-[142px]";
-    default:
-      throw new Error(`Unsupported type size ${size}`);
-  }
+type ImageSizeStyle = {
+  container: string;
+  image: string;
 };
-
-const getImageSizeStyle = (size: AavatarSize): string => {
+const getImageSizeStyle = (size: AavatarSize): ImageSizeStyle => {
   switch (size) {
     case "small":
-      return "w-[34px] h-[34px] p-[0.1rem]";
+      return { container: "w-9 h-9", image: "w-[34px] h-[34px] p-[0.1rem]" };
     case "medium":
-      return "w-[42px] h-[42px] p-[0.1rem]";
+      return { container: "w-11 h-11", image: "w-[42px] h-[42px] p-[0.1rem]" };
     case "large":
-      return "w-16 h-16 p-[0.2rem]";
+      return { container: "w-[68px] h-[68px]", image: "w-16 h-16 p-[0.2rem]" };
     case "xlarge":
-      return "w-[138px] h-[138px] p-[0.3rem]";
+      return {
+        container: "w-[142px] h-[142px]",
+        image: "w-[138px] h-[138px] p-[0.3rem]",
+      };
     default:
       throw new Error(`Unsupported type size ${size}`);
   }
@@ -50,9 +42,9 @@ const Aavatar = ({ image, size = "large", highlight = false }: Props) => {
     <div className={getContainerStyle(size, highlight)}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        className={`bg-white object-cover p-[0.15rem] rounded-full ${getImageSizeStyle(
-          size
-        )}`}
+        className={`bg-white object-cover p-[0.15rem] rounded-full ${
+          getImageSizeStyle(size).image
+        }`}
         src={image ?? undefined}
         alt="user profile image"
         referrerPolicy="no-referrer"
