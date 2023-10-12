2a1b8dfc9dc014bb219308ee03816bd71260e653
jungjuyoung <nwwn1004@naver.com>
2023-10-13 00:22:21 +0900

    like for route handler

--- instagram/src/app/api/auth/[...nextauth]/route.ts
+++ instagram/src/app/api/auth/[...nextauth]/route.ts
@@ -23,15 +23,22 @@ export const authOptions: NextAuthOptions = {
       })
       return true
     },
-    async session({ session }) {
+    async session({ session, token }) {
       const user = session?.user
       if (user) {
         session.user = {
           ...user,
+          id: token.id as string,
           username: user.email?.split('@')[0] || ''
         }
       }
       return session
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
     }
   },
   pages: {
--- instagram/src/service/posts.ts
+++ instagram/src/service/posts.ts
@@ -51,3 +51,16 @@ export async function getSavedPostsOf(username: string) {
 function mapPosts(posts: SimplePost[]) {
   return posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
 }
+
+export async function likePost(postId: string, userId: string) {
+  return client.patch(postId)//
+    .setIfMissing({ likes: [] })
+    .append('likes', [{ _ref: userId, _type: 'reference' }])
+    .commit({ autoGenerateArrayKeys: true })
+}
+
+export async function dislikePost(postId: string, userId: string) {
+  return client.patch(postId)//
+    .unset([`likes[_ref=="${userId}"]`])
+    .commit()
+}
